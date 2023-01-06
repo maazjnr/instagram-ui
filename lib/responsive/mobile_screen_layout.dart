6ab61fe4bfc114ac3b70b48a0909d0fe20ed94cc
maazjnr <goalmaaz@gmail.com>
2023-01-06 04:26:00 +0100

    bottom tab

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,15 +1,40 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_ui/utils/color.dart';
 
 class MobileScreenLayout extends StatelessWidget {
   const MobileScreenLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child:  Text('This is mobile'),
+    return Scaffold(
+      body: const Center(
+        child: Text('This is mobile'),
       ),
-      
+      bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: mobileBackgroundColor,
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: "",
+                backgroundColor: mobileBackgroundColor),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: "",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add_circle),
+              label: "",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: "",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: "",
+            ),
+          ]),
     );
   }
 }
