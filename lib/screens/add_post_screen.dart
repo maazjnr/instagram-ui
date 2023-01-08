3070df7a88e90f084694a93f6e73c2f0d3dce32d
maazjnr <goalmaaz@gmail.com>
2023-01-08 04:55:11 +0100

    image picker

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:instagram_ui/utils/color.dart';
+import 'package:instagram_ui/utils/utils.dart';
 
 class AddPostScreen extends StatefulWidget {
   const AddPostScreen({super.key});
@@ -14,6 +16,24 @@ class _AddPostScreenState extends State<AddPostScreen> {
   final muftiimage =
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfnaJnuZ4HAuDFCzMcZyxjjh6XNXfsxlO5j1vLxxOAD8M1iFu3";
 
+    _selectImage(BuildContext context) async {
+    return showDialog(context: context, builder: (context) {
+      return SimpleDialog(
+        title: Text('Create a Post'),
+        children: [
+          SimpleDialogOption(
+            padding: const EdgeInsets.all(20),
+            child: const Text('Take a Photo'),
+            onPressed: () async {
+              Navigator.of(context).pop();
+              // ignore: unused_local_variable
+              Uint8List file = await pickImage(ImageSource.camera);
+            },
+          )
+        ],
+      );
+    } );
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,21 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
-
 // for picking up image from gallery
 pickImage(ImageSource source) async {
-  
   final ImagePicker _imagePicker = ImagePicker();
   XFile? _file = await _imagePicker.pickImage(source: source);
   if (_file != null) {
     return await _file.readAsBytes();
   }
   print('No Image Selected');
-}
-
-class XFile {
-  readAsBytes() {}
-  
 }
 
 // for displaying snackbars
@@ -26,24 +19,3 @@ showSnackBar(BuildContext context, String text) {
     ),
   );
 }
-
-
-
-
-  _selectImage(BuildContext context) async {
-    return showDialog(context: context, builder: (context) {
-      return SimpleDialog(
-        title: Text('Create a Post'),
-        children: [
-          SimpleDialogOption(
-            padding: EdgeInsets.all(20),
-            child: const Text('Take a Photo'),
-            onPressed: () async {
-              Navigator.of(context).pop();
-              Uint8List file = await 
-            },
-          )
-        ],
-      );
-    } );
-  }
